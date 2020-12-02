1c0396ae3e4621ca69cd823adb40ba1fd7a5144b
SpanishPear <shrey.somaiya@gmail.com>
2020-12-03 10:11:48 +1100

    handle invalid class routes

--- frontend/src/pages/tutorialPage/index.js
+++ frontend/src/pages/tutorialPage/index.js
@@ -1,31 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Box, CircularProgress } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
 import { TutorialContentCard } from '../../components';
 import API_URL from '../../api';
 
 const LandingPage = () => {
-  const [tutorials, setTutorials] = useState([]);
+  const [tutorial, setTutorial] = useState([]);
+  const { classcode } = useParams();
 
   useEffect(() => {
-    const url = `${API_URL}/tutorials`;
-    axios.get(url).then((res) => setTutorials(res.data[0]));
-  }, []);
+    const url = `${API_URL}/tutorials?classcode=${classcode}`;
+    axios.get(url).then((res) => setTutorial(res.data[0]));
+  }, [classcode]);
 
-  return (
+  return tutorial === undefined ? (
     <Box
       display="flex"
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
     >
-      {tutorials.tutorial_contents !== undefined ? (
-        tutorials.tutorial_contents
+      404 Class not found
+    </Box>
+  ) : (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      {tutorial.tutorial_contents !== undefined ? (
+        tutorial.tutorial_contents
           .slice()
           // .reverse() -- perhaps itd be better with the most recent tut first?
           .map((data) => (
             <TutorialContentCard
-              subject={tutorials.course_code}
+              subject={tutorial.course_code}
               key={data.id}
               data={data}
             />
